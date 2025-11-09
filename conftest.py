3942ae9ecf3078c46226c774d2832db31b6cdfcb
gdev19 <ashkhen09300@gmail.com>
2025-11-09 23:43:14 +0100

    change env vars

--- conftest.py
+++ conftest.py
@@ -5,6 +5,6 @@ from os import getenv
 @fixture(scope="session")
 def testuff_client():
     return TestuffClient(
-        email=getenv("username"),
-        password=getenv("password"),
+        email=getenv("USERNAME"),
+        password=getenv("PASSWORD"),
     )
--- test_demo.py
+++ test_demo.py
@@ -7,12 +7,12 @@ from time import perf_counter
 
 def test_has_correct_title(page: Page, testuff_client: TestuffClient) -> None:
     try:
-        first_test = list(testuff_client.get(Test, id="rcxwuqqkmukewhryqun2fbp4mlxvjiuj"))
+        first_test = list(testuff_client.get(Test, id=getenv("HOME_PAGE_TEST1_ID")))
         lab_name = "Results"
         start = perf_counter()
         page.goto("https://codeff.nl/")
         expect(page).to_have_title(re.compile("Codeff"))
-        testuff_client.add_automation(token=getenv("token"), lab_name=lab_name, automation_id=first_test[0].automation_id, branch_id=first_test[0].branch_id, name="Home Page Accessible", status='passed', seconds=int(perf_counter() - start), comment="Checked with Playwright")
+        testuff_client.add_automation(token=getenv("TOKEN"), lab_name=lab_name, automation_id=first_test[0].automation_id, branch_id=first_test[0].branch_id, name="Home Page Accessible", status='passed', seconds=int(perf_counter() - start), comment="Checked with Playwright")
     except Exception as e:
-        testuff_client.add_automation(token=getenv("token"), lab_name=lab_name, automation_id=first_test[0].automation_id, branch_id=first_test[0].branch_id, name="Home Page Accessible", status='failed', seconds=int(perf_counter() - start), comment=f"Checked with Playwright. Error: {e}")
+        testuff_client.add_automation(token=getenv("TOKEN"), lab_name=lab_name, automation_id=first_test[0].automation_id, branch_id=first_test[0].branch_id, name="Home Page Accessible", status='failed', seconds=int(perf_counter() - start), comment=f"Checked with Playwright. Error: {e}")
         raise e
